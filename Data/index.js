9ea3324b0e3e51731727570a0efaabd67596afc3
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2021-01-20 10:06:29 -0600

    Added tweet mode extended

--- Data/index.js
+++ Data/index.js
@@ -8,7 +8,7 @@ const SSH_DATA = {
     host: '10.4.27.75',
     port: 22, 
     username: 'A01023646',
-    password: 'TEC.Lalongo1606',
+    password: '',
     tryKeyboard: true
 }
 const MYSQL_DATA = {
@@ -22,7 +22,8 @@ const TWITTER_DATA = {
     consumer_key: 'yV0a5aPMXLcW3aKMSvxE9jjzu',
     consumer_secret: 'CH7t2e1hEX0WY48xVP9bKq5kvriwSf5lo9NEov6j50uJRB9wC2',
     access_token_key: '57733058-dMO5On1f6s1OfCPUSSSgSOScTlDmJbzGFaqgl6VbH',
-    access_token_secret: '3PGA7ukcygjWy5s4aiilMVi0nhGuAJH1oQjgRTXIOeLXk'
+    access_token_secret: '3PGA7ukcygjWy5s4aiilMVi0nhGuAJH1oQjgRTXIOeLXk',
+    tweet_mode='extended'
 }
 
 /**
