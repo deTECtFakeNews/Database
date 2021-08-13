a78204fba851fca88d4c22b84d206c5a88c14405
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2021-08-13 16:47:33 -0500

    Merge

--- scripts/query-executor-single.js
+++
@@ -1,9 +0,0 @@
-const Connection = require("../Data");
-const QueryModel = require("../Models_new/Query/QueryModel");
-const QueryService = require("../Services/Query/QueryService");
-
-Connection.Database.connect().then(async ()=>{
-    const [q] = await QueryService.read(process.argv[2])
-    const query = new QueryModel(q);
-    await query.executeHistoric();
-})
