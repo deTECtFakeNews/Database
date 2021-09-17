96995bfe7b04f5e2a5cd2885e7e8db0fc6e9ed9e
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2021-09-17 03:04:23 -0500

    Preping for analysis

--- Models/User/UserRelationAnalysisModel.js
+++ Models/User/UserRelationAnalysisModel.js
@@ -13,23 +13,27 @@ class UserRelationAnalysisModel{
     simTopics;
     simProfile;
     constructor(aUserID, bUserID){
-        this.aUser = new UserModel(aUserID);
-        this.bUser = new UserModel(bUserID);
+        this.aUser = new UserModel({userID: aUserID});
+        this.bUser = new UserModel({userID: bUserID});
     }
     async getSimCommunity(){
+        // TODO: Improve performance
         let communityA = await UserRelationAnalysisService.getCommunity(this.aUser.userID);
         let communityB = await UserRelationAnalysisService.getCommunity(this.bUser.userID);
-        let sharedDirectCommunity = [...communityA, ...communityB];
-        // intersection(communityA, communityB)/(union(community A, community B) - intersection(communityA, communityB))
-        // repeat for 2deg (is it thaat necessary?)
+        let communityIntersection = await UserRelationAnalysisService.getCommunityIntersection(this.bUser.userID, this.aUser.userID);
+
+        this.simCommunity = communityIntersection/(communityA + communityB);
+        return this.simCommunity;
     }
     async getSimMentions(){
-        return 0.5*UserRelationAnalysisService.weightUserMentions(this.aUser.userID, this.bUser.userID)
-            + 0.5*UserRelationAnalysisService.weightUserMentions(this.bUser.userID, this.aUser.userID);
+        this.simMentions = 0.5*(await UserRelationAnalysisService.weightUserMentions(this.aUser.userID, this.bUser.userID))
+            + 0.5*(await UserRelationAnalysisService.weightUserMentions(this.bUser.userID, this.aUser.userID));
+        return this.simMentions;
     }
     async getSimRetweets(){
-        return 0.5*UserRelationAnalysisService.weightUserRetweets(this.aUser.userID, this.bUser.userID) 
-            + 0.5*UserRelationAnalysisService.weightUserRetweets(this.bUser.userID, this.aUser.userID);
+        this.simRetweets = 0.5*(await UserRelationAnalysisService.weightUserRetweets(this.aUser.userID, this.bUser.userID))
+            + 0.5*(await UserRelationAnalysisService.weightUserRetweets(this.bUser.userID, this.aUser.userID));
+        return this.simRetweets;
     }
     async getSimHastags(){
         let topA = await UserRelationAnalysisService.getTopHashtags(this.aUser.userID);
@@ -45,20 +49,70 @@ class UserRelationAnalysisModel{
         let samePlaces = this.aUser.placeDescription != undefined && this.bUser.placeDescription != undefined && this.aUser.placeDescription == this.bUser.placeDescription;
         // Get dates
         let dateWeight = Math.min( new Date() - this.aUser.creationDate, new Date() - this.bUser.creationDate ) / Math.max( new Date() - this.aUser.creationDate, new Date() - this.bUser.creationDate );
-        let result = (sameLanguages ? 1 : 0) + (samePlaces ? 1 : 0) + dateWeight;
-        return result/3;
+        this.simProfile = ((sameLanguages ? 1 : 0) + (samePlaces ? 1 : 0) + dateWeight)/3;
+        return this.simProfile;
     }
-    async getResult({ weightSimCommunity, weightSimMentions, weightSimRetweets, weightSimHashtags, weightSimLikes, weightSimTopics, weightSimProfile }){
+    getJSON(){
+        return{
+            aUser: this.aUser.userID, 
+            bUser: this.bUser.userID, 
+            simCommunity: this.simCommunity,
+            simMentions: this.simMentions, 
+            simRetweets: this.simRetweets,
+            simLikes: this.simLikes,
+            simHashtags: this.simHashtags,
+            simTopics: this.simTopics,
+            simProfile: this.simProfile
+        }
+    }
+    async getAll(){
+        await Promise.all([
+            this.getSimCommunity(),
+            this.getSimMentions(), 
+            this.getSimRetweets(),
+            this.getSimProfile()
+        ])
+    }
+
+    /* async getResult({ weightSimCommunity, weightSimMentions, weightSimRetweets, weightSimHashtags, weightSimLikes, weightSimTopics, weightSimProfile }){
         return weightSimCommunity*(await this.getSimCommunity()) + 
             weightSimMentions*(await this.getSimMentions()) + 
             weightSimRetweets*(await this.getSimRetweets()) + 
             weightSimHashtags*(await this.weightSimHashtags()) + 
             weightSimProfile*(await this.getSimProfile());
-    }
+    } */
     async uploadToDatabase(){
 
     }
     async verifyWithAPI(){
-        
+
     }
 }
+Connection.Database.connect().then(async ()=>{
+    Connection.connections['user-stats-read'].query(`
+        SELECT userID FROM view_UserStatsLast
+        WHERE view_UserStatsLast.followersCount >= 10000
+    `).on('result', async significantUser => {
+        Connection.connections['user-stats-write'].query(`
+            SELECT userID FROM view_UserStatsLast
+            WHERE view_UserStatsLast.followersCount > 10000
+                AND view_UserStatsLast.followersCount >= 1000
+        `).on('result', async otherUser => {
+            Connection.connections['user-stats-write'].pause()
+            console.log(significantUser.userID, otherUser.userID);
+            let model = new UserRelationAnalysisModel(significantUser.userID, otherUser.userID);
+            await model.getAll();
+            console.log(model.getJSON())
+            Connection.connections['user-stats-write'].resume()
+        }).on('error', console.log)
+    }).on('error', console.log)
+    
+    
+    // await model.getAll();
+    // console.log(model.getJSON());
+    /* console.log("Sim community", await model.getSimCommunity());
+    let model = new UserRelationAnalysisModel('1248695848298373120', '165832833');
+    console.log("Sim mentions", await model.getSimMentions());
+    console.log("Sim retweets", await model.getSimRetweets()); */
+    // model.getSimCommunity();
+})
--- Services/User/UserRelationAnalysisService.js
+++ Services/User/UserRelationAnalysisService.js
@@ -1,21 +1,74 @@
 const Connection = require("../../Data");
 
+const create = (userAnalysisJSON) => new Promise((resolve, reject) => {
+    if(userAnalysisJSON.aUserID == undefined || userAnalysisJSON.aUserID == -1) return resolve();
+    if(userAnalysisJSON.bUserID == undefined || userAnalysisJSON.bUserID == -1) return resolve();
+    Connection.connections['user-followers-write'].query('INSERT INTO UserRelationAnalysis SET ?', userAnalysisJSON, (error, results, fields) => {
+        // Reject on error
+        if(error && error.code != 'ER_DUP_ENTRY') reject(error);
+        // Else return newly aaded row
+        else resolve(userAnalysisJSON);
+    })
+})
+
+/**
+ * Database - Gets community size (unique followers + followed) of a user
+ * @param {String} userID Id of user to get community
+ * @returns {Promise<Number>}
+ */
 const getCommunity = userID => new Promise((resolve, reject) => {
-    Connection.connections['user-follower-read'].query(`
-        SELECT DISTINCT userID FROM (
+    Connection.Database.connections['user-followers-read'].query(`
+        SELECT COUNT(DISTINCT a.userID) AS count FROM (
             SELECT 
-                followerID 
+                followerID as userID
             FROM UserFollower 
-            WHERE ?
+            WHERE userID=${userID}
             UNION
             SELECT
-                userID
+                userID as userID
             FROM UserFollower
-            WHERE ?
+            WHERE followerID=${userID}
         ) AS a
-    `,  [userID, userID], (error, results, fields) => {
+    `, (error, results, fields) => {
         if(error) reject(error);
-        resolve([...new Set(results)]);
+        resolve(results[0].count)
+    })
+})
+
+const getCommunityIntersection = (aUserID, bUserID) => new Promise((resolve, reject) => {
+    Connection.Database.connections['user-followers-read'].query(`
+        SELECT COUNT(*) as count FROM 
+        (
+            SELECT DISTINCT userID FROM (
+                SELECT 
+                    followerID as userID
+                FROM UserFollower 
+                WHERE userID=${aUserID}
+                UNION
+                SELECT
+                    userID as userID
+                FROM UserFollower
+                WHERE followerID=${aUserID}
+            ) AS a
+        ) AS community_a
+        JOIN
+        (
+            SELECT DISTINCT userID FROM (
+                SELECT 
+                    followerID as userID
+                FROM UserFollower 
+                WHERE userID=${bUserID}
+                UNION
+                SELECT
+                    userID as userID
+                FROM UserFollower
+                WHERE followerID=${bUserID}
+            ) AS a
+        ) AS community_b
+        USING (userID)
+    `, (error, results, fields) => {
+        if(error) reject(error);
+        resolve(results[0].count);
     })
 })
 
@@ -31,17 +84,23 @@ const getCommunity2Deg = userID => new Promise(async (resolve, reject) => {
     resolve([...new Set(users)])
 })
 
+/**
+ * Database - Gets mentions weighted by amount of mentions in tweet and amount of mentions from user
+ * @param {String} aUserID Id of user creating the mentions
+ * @param {String} bUserID Id of user being mentioned
+ * @returns {Promise<Number>}
+ */
 const weightUserMentions = (aUserID, bUserID) => new Promise((resolve, reject) => {
     let value = 0;
     let mentionCount = 0;
     let tweetIDs = new Set();
-    Connection.connections['tweet-entities-read'].query(`
+    let q = Connection.Database.connections['tweet-entities-read'].query(`
         SELECT 
             * 
         FROM view_UserMentions 
         JOIN view_TweetEntitiesStats USING (tweetID)
-        WHERE ?
-    `, {authorID: aUserID, userID: bUserID})
+        WHERE authorID = ${aUserID} AND userID = ${bUserID}
+    `)
         .on('result', row=>{
             if(tweetIDs.has(row.tweetID)) return;
             tweetIDs.add(row.tweetID);
@@ -52,23 +111,41 @@ const weightUserMentions = (aUserID, bUserID) => new Promise((resolve, reject) =
             if(mentionCount == 0) resolve(0);
             resolve(value/mentionCount);
         })
-        .on('error', reject);
+        .on('error', (e)=>{
+            console.log(q.sql)
+            reject(e)
+        });
 })
 
+/**
+ * Database - Get retweets of a from b
+ * @param {String} aUserID Id of user making the retweet
+ * @param {String} bUserID Id of original author
+ * @returns {Promise<Number>}
+ */
 const weightUserRetweets = (aUserID, bUserID) => new Promise((resolve, reject) => {
     let retweets = 0;
-    Connection.connections['tweet-retweet-read'].query(`
+    Connection.Database.connections['tweet-retweets-read'].query(`
         SELECT
-            *
-        FROM view_UserRetweet
-        WHERE ?
-    `, {authorID: bUserID, retweeterID: aUserID})
+            Tweet.authorID, 
+            Tweet.tweetID, 
+            TweetRetweet.authorID as retweeterID
+        FROM TweetRetweet
+        JOIN Tweet USING (tweetID)
+        WHERE Tweet.authorID = ${bUserID} AND TweetRetweet.authorID = ${aUserID}
+    `,)
         .on('result', row=>{
             retweets++;
         })
         .on('end', row=>{
             if(retweets == 0) resolve(0);
-            Connection.connections['tweet-retweet-write'].query(`SELECT COUNT(*) AS count FROM view_UserRetweet WHERE`, {retweeterID: aUserID})
+            Connection.connections['tweet-retweets-write'].query(`
+                SELECT
+                    COUNT(*) AS count
+                FROM TweetRetweet
+                JOIN Tweet USING (tweetID)
+                WHERE TweetRetweet.authorID = ${aUserID}
+            `)
                 .on('result', r=>{
                     resolve(retweets/r.count)
                 })
@@ -78,7 +155,7 @@ const weightUserRetweets = (aUserID, bUserID) => new Promise((resolve, reject) =
 })
 
 const getTopHashtags = (userID) => new Promise((resolve, reject) => {
-    Connection.connections['tweet-entities-write'].query(`
+    Connection.Database.connections['tweet-entities-write'].query(`
         SELECT
             *,
             COUNT(*) as count
@@ -93,5 +170,5 @@ const getTopHashtags = (userID) => new Promise((resolve, reject) => {
     })
 })
 
-const UserRelationAnalysisService = {getCommunity, getCommunity2Deg, weightUserMentions, weightUserRetweets, getTopHashtags};
+const UserRelationAnalysisService = {getCommunity, getCommunityIntersection, getCommunity2Deg, weightUserMentions, weightUserRetweets, getTopHashtags};
 module.exports = UserRelationAnalysisService;
