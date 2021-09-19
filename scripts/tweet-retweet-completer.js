bb7971ded3d5145a85a5a0aab8760bfa934304e2
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2021-09-19 03:11:21 -0500

    Added buffer

--- scripts/tweet-retweet-completer.js
+++ scripts/tweet-retweet-completer.js
@@ -1,6 +1,27 @@
 const Connection = require("../Data");
 const TweetModel = require("../Models/Tweet/TweetModel");
 
+
+class TweetRetweetBuffer extends Array {
+    maxSize;
+    constructor(maxSize){
+        super();
+        this.maxSize = maxSize;
+    }
+    async push(tweetRetweet){
+        super.push(tweetRetweet);
+        if(this.length >= this.maxSize){
+            await this.execute();
+            this.length = 0;
+        }
+    }
+    async execute(){
+        await Promise.all(this.map(element => element.getFromAPIAndUploadToDatabase()))
+    }
+}
+
+let buffer = new TweetRetweetBuffer(20);
+
 Connection.Database.connect().then(()=>{
     Connection.connections['tweet-main-read'].query(`
         SELECT
@@ -15,7 +36,8 @@ Connection.Database.connect().then(()=>{
         try{
             await tweet.latestStats.getFromDatabase();
             console.log(tweet.tweetID, tweet.latestStats.last().getJSON());
-            await tweet.retweets.getFromAPIAndUploadToDatabase();
+            // await tweet.retweets.getFromAPIAndUploadToDatabase();
+            await buffer.push(tweet.retweets)
             console.log(tweet.tweetID, 'Success')
         } catch(e){
             console.log(tweet.tweetID, 'Error', e);
