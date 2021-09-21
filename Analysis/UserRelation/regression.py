a76b834c27bff57d5d97903f1e2881d5178fc1b9
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2021-09-21 03:31:00 -0500

    Add ml model

--- Analysis/UserRelation/regression.py
+++ Analysis/UserRelation/regression.py
@@ -2,37 +2,70 @@
 # logistic regression to determine the coefficents and store them in 
 # UserRelationshipCoefficients table
 
-# import mysql.connector
+# COnnection to db
+from operator import mod
 import pymysql
+import pymysql.cursors
 import sshtunnel
+
+# Dataframes
 import pandas as pd
-import logging
-import time
+
+# ML
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+tunnel = 0
+database : pymysql.Connection = 0
 
 def connectToDatabaseOverSSH():
-    with sshtunnel.SSHTunnelForwarder(
+    global tunnel
+    global database
+    tunnel = sshtunnel.SSHTunnelForwarder(
         ('prod022ws08.itesm.mx', 22), 
         ssh_username='A01023646',
         ssh_password='TEC.Lalongo1606', 
         remote_bind_address=('prod022ws08.itesm.mx', 3306)
-    ) as tunnel:
-        tunnel.start()
-        print("Connected to SSH Tunnel")
-        database = pymysql.connect(
-                host='localhost', 
-                user='DataUser', 
-                password='TEC.F4keNews', 
-                database='FakeNews', 
-                port=tunnel.local_bind_port,
-                max_allowed_packet=67108864,
-                autocommit=True
-        )
-        print("Connected to Database")
-        return database
+    )
+    tunnel.start()
+    print("Connected to SSH Tunnel")
+    database = pymysql.connect(
+            host='localhost', 
+            user='DataUser', 
+            password='TEC.F4keNews', 
+            database='FakeNews', 
+            port=tunnel.local_bind_port,
+            max_allowed_packet=1073741824,
+            
+    )
+    print("Connected to Database")
 
-with connectToDatabaseOverSSH() as database:
-    cursor = database.cursor()
-    cursor.execute("SELECT VERSION()")
-#    data = pd.read_sql('SELECT * FROM UserRelationAnalysis WHERE verificationResult IS NOT NULL LIMIT 20', con=database)
-#    data.head()
+def trainModel(df):
+    X_train, X_test, y_train, y_test = train_test_split(df[['simCommunity', 'simMentions', 'simRetweets', 'simHashtags', 'simProfile']], df.verificationResult)
+    model = LogisticRegression(max_iter=100000)
+    model.fit(X_train, y_train)
+
+    results = {
+        'simCommunity': model.coef_[0][0],
+        'simMentions': model.coef_[0][1], 
+        'simRetweets': model.coef_[0][2], 
+        'simHashtags': model.coef_[0][3], 
+        'simProfile': model.coef_[0][4], 
+        'score': model.score(X_test, y_test)
+    }
+    return results
+
+connectToDatabaseOverSSH()
+cursor : pymysql.cursors.Cursor = database.cursor()
+try:
+    data = pd.read_sql("""
+        SELECT * FROM  UserRelationAnalysis WHERE verificationResult IS NOT NULL
+    """, con=database)
+    # print(data.head())
+    print(trainModel(data))
+except:
+    print("Error")
+finally:
+    cursor.close()
+    tunnel.close()
 
